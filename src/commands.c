7dbfa4dc9ec71ae1ee2bcc54adaf4dced641e6da
Changhyun Kim <chang9403@ajou.ac.kr>
2017-11-12 23:49:37 +0900

    Process Creation Complete

--- src/commands.c
+++ src/commands.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "commands.h"
 #include "built_in.h"
@@ -50,8 +54,27 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
     } else if (strcmp(com->argv[0], "exit") == 0) {
       return 1;
     } else {
-      fprintf(stderr, "%s: command not found\n", com->argv[0]);
-      return -1;
+      pid_t c_pid, pid;
+      int status;
+      c_pid = fork();
+
+      if(c_pid==0){
+        pid = getpid();
+        execv(com->argv[0], com->argv);
+        fprintf(stderr, "%s: command not found\n", com->argv[0]);
+	return 1;
+      }
+      else if(c_pid>0){
+        pid = wait(&status);
+      }
+      else{
+        fprintf(stderr, "Fork failed");
+        exit(1);
+	return -1;
+      }
+      return 0;
+ //     fprintf(stderr, "%s: command not found\n", com->argv[0]);
+ //     return -1;
     }
   }
 
