70d61c5869a19d4077228d7677a56835a34e4f81
Changhyun Kim <chang9403@ajou.ac.kr>
2017-11-16 18:48:56 +0900

    IPC communication

--- src/commands.c
+++ src/commands.c
@@ -3,13 +3,29 @@
 #include <string.h>
 #include <assert.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define UNIX_PATH_MAX 108
+#define SOCK_PATH "tpf_unix_sock.server"
+#define DATA "Hello from server"
+
+#define SERVER_PATH "tpf_unix_sock.server"
+#define CLIENT_PATH "tpf_unix_sock.client"
 
 #include <sys/types.h>
 #include <sys/wait.h>
 
 #include "commands.h"
 #include "built_in.h"
-
+/*
+struct sockaddr_un {
+  unsigned short int sun_family;
+  char sun_path[UNIX_PATH_MAX];
+};
+*/
 static struct built_in_command built_in_commands[] = {
   { "cd", do_cd, validate_cd_argv },
   { "pwd", do_pwd, validate_pwd_argv },
@@ -29,12 +45,74 @@ static int is_built_in_command(const char* command_name)
   return -1; // Not found
 }
 
+void* runner(void* temp)
+{
+  int client_sock, rc, len;
+  struct sockaddr_un server_sockaddr;
+  struct sockaddr_un client_sockaddr;
+  char buf[256];
+  memset(&server_sockaddr, 0, sizeof(struct sockaddr_un));
+  memset(&client_sockaddr, 0, sizeof(struct sockaddr_un));
+
+  client_sockaddr.sun_family = AF_UNIX;
+  strcpy(client_sockaddr.sun_path, CLIENT_PATH);
+  len = sizeof(client_sockaddr);
+
+  client_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+  if(client_sock == -1){
+    printf("SOCKET ERROR\n");
+    exit(1);
+
+  }
+
+  unlink(CLIENT_PATH);
+  rc = bind(client_sock, (struct sockaddr *) &client_sockaddr, len);
+  if(rc == -1){
+    printf("CLIENT BIND ERROR: \n");
+    close(client_sock);
+    exit(1);
+
+    }
+  else
+  {
+    printf("Client Bind SUCCESS\n");
+  }
+
+  server_sockaddr.sun_family = AF_UNIX;
+  strcpy(server_sockaddr.sun_path, SERVER_PATH);
+  rc = connect(client_sock, (struct sockaddr *) &server_sockaddr, len);
+  if(rc == -1){
+  printf("CONNECT ERROR\n");
+  close(client_sock);
+  exit(1);
+  }
+  else
+  {
+   printf("Client Connect Success\n");
+  }
+
+  strcpy(buf, "Hi Im client");
+  printf("Sending DATA...\n");
+  rc = send(client_sock, buf, sizeof(buf), 0);
+
+  if(rc == -1){
+    printf("SEND ERROR\n");
+    close(client_sock);
+    exit(1);
+  }
+  else{
+    printf("data sent\n");
+  }
+  close(client_sock);
+  pthread_exit(0);
+}
+
 /*
  * Description: Currently this function only handles single built_in commands. You should modify this structure to launch process and offer pipeline functionality.
  */
 int evaluate_command(int n_commands, struct single_command (*commands)[512])
 {
-  if (n_commands > 0) {
+  if (n_commands ==  1) {
     struct single_command* com = (*commands);
 
     assert(com->argc != 0);
@@ -53,7 +131,9 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
       return 0;
     } else if (strcmp(com->argv[0], "exit") == 0) {
       return 1;
-    } else {
+    }
+      
+      else {
       pid_t c_pid, pid;
       int status;
       c_pid = fork();
@@ -77,6 +157,101 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
  //     return -1;
     }
   }
+  else if (n_commands == 2) {
+      int server_sock, client_sock, len, rc;
+      int bytes_rec = 0;
+      struct sockaddr_un server_sockaddr;
+      struct sockaddr_un client_sockaddr;
+      char buf[256];
+      int backlog = 10;
+      memset(&server_sockaddr, 0, sizeof(struct sockaddr_un));
+      memset(&client_sockaddr, 0, sizeof(struct sockaddr_un));
+      memset(buf, 0, 256);
+
+      server_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+      if (server_sock == -1){
+        printf("SOCKET ERROR\n");
+        exit(1);
+      }
+      else
+      {
+        printf("Server program Server socket making success\n");
+      }
+
+      server_sockaddr.sun_family = AF_UNIX;
+      strcpy(server_sockaddr.sun_path, SOCK_PATH);
+      len = sizeof(server_sockaddr);
+
+      unlink(SOCK_PATH);
+      rc = bind(server_sock, (struct sockaddr *) &server_sockaddr, len);
+      if(rc == -1){
+        printf("SERVER BIND ERROR:\n");
+        close(server_sock);
+        exit(1);
+      }
+      else
+      {
+       printf("Server program Bind Success\n");
+      }
+      
+      pthread_t tid;
+      int temp =0;
+      //Fix needed
+      pthread_create(&tid, NULL, runner, &temp);
+   
+      rc = listen(server_sock, backlog);
+      if(rc == -1){
+        printf("LISTEN ERROR:\n");
+        close(server_sock);
+        exit(1);
+      }
+     
+      printf("server is listening...\n");
+
+      pthread_join(tid,NULL);
+
+      client_sock = accept(server_sock, (struct sockaddr *) &client_sockaddr, &len);
+      if(client_sock == -1){
+        printf("ACCEPT ERROR:\n");
+        close(server_sock);
+        close(client_sock);
+        exit(1);
+      }
+      else
+      {
+        printf("server program accept success\n");
+      }
+
+      len = sizeof(client_sockaddr);
+      rc = getpeername(client_sock, (struct sockaddr *) &client_sockaddr, &len);
+      if(rc == -1){
+        printf("GETPEERNAME ERROR:\n");
+        close(server_sock);
+        close(client_sock);
+        exit(1);
+      }
+      else {
+        printf("Client socket filepath: %s\n", client_sockaddr.sun_path);
+      }
+
+      memset(buf, 0, 256);
+      printf("receving data...\n");
+      rc = recv(client_sock, buf, sizeof(buf), 0);
+      if(rc == -1) {
+        printf("RECEIVE ERROR: \n");
+        close(server_sock);
+        close(client_sock);
+        exit(1);
+      }
+      else {
+      printf("Data receve!\n");
+      printf("Data : %s", buf);
+      }
+
+      close(server_sock);
+      close(client_sock);
+
+    }
 
   return 0;
 }
